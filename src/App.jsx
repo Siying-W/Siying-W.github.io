c2fe9ecf8cc0a2a1cca90ccc9bb3e45fdca26ec4
b9d8f <email@domain.com>
2025-08-17 15:03:47 -0700

    Changed BrowserRouter to HashRouter in src/App.jsx Removed the base path from vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import About from './components/About';
 import BeyondResearch from './components/BeyondResearch';
 import Contact from './components/Contact';
